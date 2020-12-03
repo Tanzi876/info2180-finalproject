8091d24e3b44393410a948b349ec8e39bf68bbe8
BeaTea109 <55596758+BeaTea109@users.noreply.github.com>
2020-12-02 19:27:55 -0500

    Update schema.sql
    Updated MYSQL DB Schema for the project and added initial admin user 
    
    nb. For the project use MD5 hashing

--- schema.sql
+++ schema.sql
@@ -1,23 +1,37 @@
-Create table USER(
-    'id' int(11) not null auto_increment,
-    'firstname' varchar(30) not null,
-    'lastname' varchar(30) not null,
-    'password' varchar(30) not null,
-    'email' varchar(30) not null,
-    'date_joined' datetime,
-    primary key('id')
+DROP TABLE IF EXISTS `Issue`;
 
-)
-create table ISSUE(
-     'id' int(11) not null auto_increment,
-     'title' varchar(30) not null,
-     'description' text(100) no null,
-     'type' varchar(30) not null,
-     'priority' varchar(30) not null,
-     'status' varchar(30) not null,
-     'assigned_to' int(30),
-     'created_by ' int(30),
-     'created' datetime
-     'update' datetime
+DROP TABLE IF EXISTS `User`;
 
-)
+CREATE TABLE `User` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`firstname` varchar(255) NOT NULL,
+	`lastname` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	`date_joined` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+);
+CREATE TABLE `Issue` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`title` varchar(255),
+	`description` TEXT(255),
+	`type` varchar(255),
+	`priority` varchar(255),
+	`status` varchar(255),
+	`assigned_to` int NOT NULL,
+	`created_by` int NOT NULL,
+	`created` DATETIME NOT NULL,
+	`updated` DATETIME NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+
+
+/* Foreign keys creation for Created_By and Assigned_To to be referenced in the User table */
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_fk0` FOREIGN KEY (`assigned_to`) REFERENCES `User`(`id`);
+
+ALTER TABLE `Issue` ADD CONSTRAINT `Issue_fk1` FOREIGN KEY (`created_by`) REFERENCES `User`(`id`);
+
+
+/* Initialize Admin Credentials (Password hashed using MD5) */
+INSERT INTO User(firstname,lastname, password, email, date_joined) values('Admin', ' ', 'afc285bebb3dd733796cb06db01cd59a', 'admin@project2.com', '2020-12-01');
