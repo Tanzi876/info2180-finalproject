d3efa31fe9dd8bf582372719eee1631e800049f6
Marlon Lewis <marlonlewis483@gmail.com>
2020-12-05 19:24:28 -0500

    Added InsertIssue.css, InsertIssue.html and InsertIssue.php files. PHP still not adding to database as yet.

--- InsertIssue.php
+++ InsertIssue.php
@@ -1,9 +1,9 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=VoteLogDB";
-$username = "comp2190SA";
-$password = "2020Sem1";
+$dsn = 'mysql:host=localhost;dbname=bugme';
+$username = 'admin';
+$password = 'admin';
 
-$query = $_POST[''];
+//$query = $_POST[''];
 
 if(isset($_POST['submit'])){
     try{
@@ -21,19 +21,25 @@ if(isset($_POST['submit'])){
     $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
     $type = filter_var($_POST['type'], FILTER_SANITIZE_STRING);
     $priority = filter_var($_POST['priority'], FILTER_SANITIZE_STRING);
-    $status = filter_var($_POST['status'], FILTER_SANITIZE_STRING);
-    $assigned_to = filter_var($_POST['assigned_to'], FILTER_SANITIZE_STRING);
+    $status = filter_var('Open', FILTER_SANITIZE_STRING);
+    $assigned_to = filter_var(1, FILTER_SANITIZE_NUMBER_INT);
     $created_by = filter_var($_POST['created_by'], FILTER_SANITIZE_STRING);
+    $created = date('Y-m-d H:i:s');
     
     // mysql query to insert data
 
-    $pdoQuery = "INSERT INTO ISSUE (title, description, type, priority, status, assigned_to, created_by)
-    VALUES (:title,:description,:type,:priority,:status,:assigned_to,:created_by)";
+    $pdoQuery = 'INSERT INTO ISSUE (title, description, type, priority, status, assigned_to, created_by, created)
+    VALUES (:title,:description,:type,:priority,:status,:assigned_to,:created_by)';
+
+    /*
+    INSERT INTO `issue`(`id`, `title`, `description`, `type`, `priority`, `status`, `assigned_to`, `created_by`, `created`, `updated`) 
+    VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
+    */
     
     $pdoResult = $conn->prepare($pdoQuery);
     
-    $pdoExec = $pdoResult->execute(array(":title"=>$title,":description"=>$description,":type"=>$type,":priority"=>$priority,
-    ":status"=>$status,":assigned_to"=>$assigned_to,":created_by"=>$created_by));
+    $pdoExec = $pdoResult->execute(array(':title'=>$title,':description'=>$description,':type'=>$type,':priority'=>$priority,
+    ':status'=>$status,':assigned_to'=>$assigned_to,':created_by'=>$created_by,':created'=>$created));
     
         // check if mysql insert query successful
     if($pdoExec)
