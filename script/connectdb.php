61f9afa0795ae9ddbae5396beda66c65fda294b5
Tanya-Lee Lawrence <57540241+Tanzi876@users.noreply.github.com>
2020-12-06 12:19:58 -0500

    Update connectdb.php

--- script/connectdb.php
+++ script/connectdb.php
@@ -5,6 +5,7 @@
         $password = 'admin';
         $dbname = 'BugMe';
         $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
 
     }
