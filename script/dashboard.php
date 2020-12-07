6ff989a7e38c013245e62894eaef34a7c7a33085
Tanzi876 <tanyaleelawrence@gmail.com>
2020-12-06 22:17:41 -0500

    Error

--- script/dashboard.php
+++ script/dashboard.php
@@ -8,7 +8,7 @@
     $my_tickets=$_GET['my_tickets'];
     
     
-        if(isset($all)){
+        if(array_key_exists('all',$_POST)){
             $conn = connectdb();
             $stmt = $conn->query("SELECT * FROM Issue");
             $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -21,7 +21,7 @@
     
            
         }
-        if(isset($open)){
+        if(array_key_exists('open',$_POST)){
             $conn = $conn = connectdb();
             $stmt = $conn->query("SELECT * FROM Issue where status ='open'");
             $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -34,7 +34,7 @@
             
         }
     
-        if(isset($my_tickets)){
+        if(array_key_exists('my_tickets',$_POST)){
             $conn = $conn = connectdb();
             $user=$_SESSION['userID'];
             $stmt = $conn->query("SELECT * FROM Issue where assigned_to like '% $user%'");
